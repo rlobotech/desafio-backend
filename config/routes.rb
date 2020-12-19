a1fcb1876d0944c67d34fbcef015d64d0bb88340
RAFAEL_LOBO <rlobotech@gmail.com>
2020-12-19 00:08:41 -0300

    add view for new deputies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'deputados#index', as: 'home'
+  root 'deputies#index', as: 'home'
 
-  resources :deputados
+  resources :deputies
 end
