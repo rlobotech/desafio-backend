d6bad2326789efffa0e42318870a197ab45ff117
RAFAEL_LOBO <rlobotech@gmail.com>
2020-12-19 16:20:59 -0300

    create deputy page in react

--- app/javascript/components/deputies/deputies.js
+++ app/javascript/components/deputies/deputies.js
@@ -1,15 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import DeputyCard from './deputy_card'
 
 export default function Deputies() {
   const [deputies, setDeputies] = useState([])
 
   useEffect(() => {
-    axios.get('deputies')
-    .then( resp => console.log(resp) )
-    .catch( resp => console.log(resp) )
+    axios.get('/api/v1/deputies')
+    .then(resp => {
+      setDeputies(resp.data)
+    })
+    .catch(resp => console.log(resp))
   }, [deputies.length])
 
+  const list = deputies.map(item => {
+    return(
+      <DeputyCard
+        key={item.id}
+        deputy={item}
+      />
+    )
+  })
+
   return(
-    <div>Deputies Page</div>
+    <div className="deputies container">
+      <div className="header">
+        <h1>Deputados</h1>
+        <div className="sub-header">Lista dos Deputados</div>
+      </div>
+      <div className="grid">
+        {list}
+      </div>
+    </div>
   )
 }
