d1f531fd3021b6fec6473aef32f0956bdab862b0
RAFAEL_LOBO <rlobotech@gmail.com>
2020-12-20 18:04:54 -0300

    add modal for importation

--- app/javascript/components/importations/importations_page.jsx
+++ app/javascript/components/importations/importations_page.jsx
@@ -2,28 +2,44 @@ import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import BootstrapTable from 'react-bootstrap-table-next'
 import Layout from '../layouts/layout'
+import SimpleModal from '../modals/simple_modal'
 import '../../../assets/stylesheets/importations/importations.css'
 
 export default function ImportationsPage() {
   const [selectedFile, setSelectedFile] = useState(null)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalTitle] = useState("Importação do CSV")
+  const [modalBody, setModalBody] = useState("")
 
   const onChangeHandler = event => {
-    console.log(event.target.files[0])
     setSelectedFile(event.target.files[0])
   }
 
+  const closeModal = () => setModalOpen(false)
+
   const onClickHandler = () => {
+    setModalBody("Importação dos deputados com suas notas fiscais em andamento.\nEste processo pode levar alguns minutos.\nAguarde!")
+    setModalOpen(true)
+
     const data = new FormData()
     data.append('file', selectedFile)
-    axios.post("api/v1/deputies/import_csv", data, {
-      
-    }).then(res => {
-      console.log(res.statusText)
-   })
+
+    if(selectedFile) {
+      axios.post("api/v1/deputies/import_csv", data, {
+      }).then(res => {
+        setModalBody("Importação Concluída com Sucesso!")
+      }).catch(resp => {
+        console.log(resp)
+        setModalBody(`Ocorreu o seguinte error na importação:\n${resp}\nTalvez seja necessário abrir o arquivo CSV e salvar uma nova cópia.`)
+      })
+    } else {
+      setModalBody("É necessário selecionar um arquivo para fazer a importação.")
+    }
   }
 
   return (
     <Layout>
+      {SimpleModal({modalOpen, modalTitle, modalBody, closeModal})}
       <form method="post" action="#" id="#">
         <div className="form-group files">
           <label>Faça o Upload do seu Arquivo CSV</label>
