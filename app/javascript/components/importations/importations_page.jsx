b480a2269e46b31390fa538564a8a5b5bdd3e1a1
RAFAEL_LOBO <rlobotech@gmail.com>
2020-12-20 01:51:16 -0300

    add importation input

--- app/javascript/components/importations/importations_page.jsx
+++ app/javascript/components/importations/importations_page.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import BootstrapTable from 'react-bootstrap-table-next'
 import Layout from '../layouts/layout'
+import '../../../assets/stylesheets/importations/importations.css'
 
 export default function ImportationsPage() {
-  // const [deputies, setDeputies] = useState([])
+  const [selectedFile, setSelectedFile] = useState(null)
 
   // useEffect(() => {
   //   axios.get('/api/v1/deputies')
@@ -13,10 +14,30 @@ export default function ImportationsPage() {
   //   })
   //   .catch(resp => console.log(resp))
   // }, [])
+  const onChangeHandler = event => {
+    console.log(event.target.files[0])
+    setSelectedFile(event.target.files[0])
+  }
+
+  const onClickHandler = () => {
+    const data = new FormData()
+    data.append('file', selectedFile)
+    axios.post("api/v1/deputies/importations", data, {
+      
+    }).then(res => {
+      console.log(res.statusText)
+   })
+  }
 
   return (
     <Layout>
-      <div></div>
+      <form method="post" action="#" id="#">
+        <div className="form-group files">
+          <label>Faça o Upload do seu Arquivo CSV</label>
+          <input type="file" name="file" onChange={onChangeHandler}/>
+        </div>
+        <button type="button" className="btn btn-success btn-block" onClick={onClickHandler}>Upload</button>
+      </form>
     </Layout>
   )
 }
